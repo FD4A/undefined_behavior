fb4d305220c69b98a87985dcb84e6e225c3e03e2
dimon <End-ik@ya.ru>
2022-08-01 15:50:35 +0300

    add comments UB_aliasing.cpp

--- UB_aliasing.cpp
+++ UB_aliasing.cpp
@@ -3,14 +3,15 @@
 int foo(float *a, int *b)
 {
 	*b = 1;
-	*a = 0.0;
-	return *b;
+	*a = 0.0;//UB
+	return *b;//optimization to return 1 because
+	//in previous line unexcepted (ingnored) UB
 }
 
 int main()
 {
 	int x = 2;
 	float *p = reinterpret_cast<float*>(&x);
-	std::cout << foo(p,&x);
+	std::cout << foo(p,&x) << "  " << x;//1 0
 	return 0;
 }
